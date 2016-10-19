7813c09f484c5fa56c50d93e7a9e2d3f4597fe03
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2016-10-19 10:40:51 +0000

    implementado bucle for

--- src/practicagit/principal.java
+++ src/practicagit/principal.java
@@ -3,7 +3,12 @@ package practicagit;
 public class principal {
 	
 	public static void mais (String [] args){
+		int i;
+		for (i=0;n<=3;n++){
 		System.out.println("Ola, Ángel");
+		}
+		
+		
 	}
 
 }
